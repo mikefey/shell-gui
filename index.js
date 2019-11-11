2e8412aeee90e93c135f904dd25d2e3331c2c404
Mike Fey <mikefey@protonmail.com>
2019-11-10 22:08:41 -0500

    Add templates, start UI

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const yaml = require('js-yaml');
+const render = require('koa-ejs');
+const path = require('path');
+const Koa = require('koa');
 const readFiles = require('./utils/read-files');
 
-const getFileData = async () => {
+const app = new Koa();
+
+const getConfigData = async () => {
   const configFileData = await readFiles('./configs/')
-  configFileData.forEach((configFile) => {
-    console.log(JSON.stringify(yaml.safeLoad(configFile.content)));
+  let inc = 0;
+
+  return new Promise((resolve) => {
+    const sections = [];
+
+    configFileData.forEach((configFile) => {
+      sections.push(yaml.safeLoad(configFile.content));
+      inc++;
+
+      if (inc === sections.length) {
+        resolve(sections);
+      }
+    });
   });
 }
 
-getFileData();
+render(app, {
+  root: path.join(__dirname, 'templates'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: true
+});
+
+app.use(require('koa-static')(path.join(__dirname, 'static')));
+
+app.use(async ctx => {
+  const configData = await getConfigData();
+
+  await ctx.render('index', { configData });
+});
+
+app.listen(3000);
